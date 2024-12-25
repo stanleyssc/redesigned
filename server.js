f89cbc4d29d41934d763023bb5ad42bd5102742b
stanleyssc <drscookey@gmail.com>
2024-12-25 20:58:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://127.0.0.1:5500',
+    'http://127.0.0.1:5501',
     'https://naijagamer.netlify.app',
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
